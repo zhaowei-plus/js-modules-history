e425bb64f7896ca356c9f432eb2cb0d38bcd7c1a
zhaowei-plus <1215058679@qq.com>
2021-08-29 22:36:59 +0800

    update

--- amd&cmd-module/index.js
+++ amd&cmd-module/index.js
@@ -1,4 +1,12 @@
 require(['add', 'sub'], function(add, sub) {
     console.log('add:', add.add(1, 2))
     console.log('sub:', sub.sub(1, 2))
+
+    document.querySelector('#main-iframe').src = './page/login.html'
 })
+
+function loadModules(id) {
+    require(['vue', 'jquery'], function() {
+        require(id)
+    })
+}
--- amd&cmd-module/main.js
+++ amd&cmd-module/main.js
@@ -1,6 +1,8 @@
 require.config({
-    baseUrl: './',
+    baseUrl: '/',
     paths: {
-        // 'jQuery': "jQuery.min", // 实际路径为 js/lib/jQuery.min.js
+        'jQuery': "js/jquery", // 实际路径为 js/lib/jQuery.min.js
+        'Vue': "js/vue", // 实际路径为 js/lib/jQuery.min.js
     },
 })
+
